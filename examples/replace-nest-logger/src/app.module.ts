0c5d93ededf4544c5da5eceab4ec15ad4faace1a
Egor Kartashov <kartashov_egor96@mail.ru>
2024-01-04 10:36:55 +0600

    fix: examples

--- examples/replace-nest-logger/src/app.module.ts
+++ examples/replace-nest-logger/src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LogLevels, TelegramTransport } from '@wildegor/winston-telegram';
 import {
   utilities as nestWinstonModuleUtilities,
   WinstonModule,
 } from 'nest-winston';
 import * as winston from 'winston';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     WinstonModule.forRoot({
       transports: [
+        new TelegramTransport({
+          token: '6581515204:AAFECpWwl7XsNpehc0dbgWZxtg0QcVajTTg',
+          chatId: '-1002035814378',
+          levels: [LogLevels.Info],
+        }),
         new winston.transports.Console({
           format: winston.format.combine(
             winston.format.timestamp(),
--- examples/simple-log/src/index.ts
+++ examples/simple-log/src/index.ts
@@ -1,18 +1,16 @@
-import { TelegramTransport } from '../../../src';
-import { LogLevels } from '../../../src/shared/types/log.enum';
-
-const winston = require('winston');
+import { LogLevels, TelegramTransport } from '@wildegor/winston-telegram';
+import * as winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
-      new TelegramTransport({
-        levels: [LogLevels.Info],
-        token: '6581515204:AAFECpWwl7XsNpehc0dbgWZxtg0QcVajTTg', // for demo
-        chatId: '-1002035814378', // act like default chat id
-      }),
+    new TelegramTransport({
+      levels: [LogLevels.Info],
+      token: '6581515204:AAFECpWwl7XsNpehc0dbgWZxtg0QcVajTTg', // for demo
+      chatId: '-1002035814378', // act like default chat id
+    }),
   ]
 });
 
@@ -22,11 +20,11 @@ logger.log({
   message: 'Send logs to default chat',
 });
 
-logger.log(LogLevels.Info,"Sent to specific chat", {
+logger.log(LogLevels.Info, 'Sent to specific chat', {
   chat_id: '-1002035814378'
 });
 
-logger.log(LogLevels.Info, "Or send to topic!", {
+logger.log(LogLevels.Info, 'Or send to topic!', {
   chat_id: '-1001901864911',
   message_thread_id: 5
 });
